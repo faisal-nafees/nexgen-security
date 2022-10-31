7761fe08b40780c1ac815b8a6713fdb8fa8b6eb1
Faisal Nafees <mofaisal739@gmail.com>
2022-10-31 17:30:56 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,58 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// Swiper
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Navbar Toggle
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("header").addClass("black");
+  } else {
+    $("header").removeClass("black");
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primaryColor: "#1c434c",
+      primaryTint: "#496970",
+      secondaryColor: "#fcae04",
+      secondaryTint: "#fdc64f",
+      darkGray: "#868e96",
+      lightGray: "#e9ecef",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
