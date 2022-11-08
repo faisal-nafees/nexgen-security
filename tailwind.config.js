89dbbd3e15c850b55a81fb66d42405897f9daf4b
Faisal Nafees <mofaisal739@gmail.com>
2022-11-08 18:11:51 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      primaryColor: "#1c434c",
-      black: "#000",
+      primaryColor: "#000",
       secondaryColor: "#fcae04",
-      secondaryTint: "#fdc64f",
       darkGray: "#868e96",
       lightGray: "#e9ecef",
     },
